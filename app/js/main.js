01cf839618f7a536dd1bc3f657c8ce45a410c5e0
Serhii Boiko <boyko0043@gmail.com>
2020-11-16 20:55:54 +0200

    v1

--- app/js/main.js
+++ app/js/main.js
@@ -1,6 +1,9 @@
-$(function(){
 
+function copytext(elem) {
+    var value = $("<input>");
+    $("body").append(value);
+    value.val($(elem).text()).select();
+    document.execCommand("copy");
+    value.remove();
+}
 
-
-  
-});
